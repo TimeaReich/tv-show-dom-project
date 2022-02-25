633381f7e8d83e05ac92851323c93087b8bc5c55
Timea <timea.reich@gmail.com>
2022-02-25 22:06:05 +0000

    in progress level 200

--- script.js
+++ script.js
@@ -1,27 +1,20 @@
-//You can edit ALL of the code here
-// function setup() {
-//   const allShows = getAllShows();
-//   makePageForShows(allShows);
-// }
-
-// const { link } = require("fs");
-
-// function makePageForShows(episodeList) {
-//   const rootElem = document.getElementById("root");
-//   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
-// }
-
-// window.onload = setup;
-
-let allShows = getAllShows();
+let allEpisodes = getAllEpisodes();
 
 let body = document.querySelector("body");
-body.style.backgroundColor = "black";
-body.style.color = "white";
-
+body.className = "body";
+let searchDiv = document.createElement("div");
+body.appendChild(searchDiv);
 let feature = document.createElement("div");
 body.append(feature);
 feature.className = "feature";
+
+/////////search bar///////////
+
+let searchBar = document.createElement("input");
+searchBar.setAttribute("type", "text");
+searchBar.setAttribute("name", "search");
+searchBar.setAttribute("placeholder", "Search for series");
+searchDiv.appendChild(searchBar);
 
 //////////////episode structure///////////
 
@@ -34,39 +27,50 @@ function displayEpisodes(shows) {
     /////create title//////
     let episodeTitle = document.createElement("p");
     container.appendChild(episodeTitle);
-    episodeTitle.innerHTML = `${allShows[i].name}`;
+    episodeTitle.innerHTML = `${shows[i].name}`;
     episodeTitle.className = "title";
 
     //////create episode and season number//////
     let episodeNumber = document.createElement("p");
-    if (getOneEpisode().season >= 10) {
-      if (getOneEpisode().number >= 10) {
-        episodeNumber.innerHTML = `S${getOneEpisode().season}E${
-          getOneEpisode().number
-        }`;
+    if (shows[i].season >= 10) {
+      if (shows[i].number >= 10) {
+        episodeNumber.innerHTML = `S${shows[i].season}E${shows[i].number}`;
       }
     } else {
-      episodeNumber.innerHTML = `S0${getOneEpisode().season}E0${
-        getOneEpisode().number
-      }`;
+      episodeNumber.innerHTML = `S0${shows[i].season}E0${shows[i].number}`;
       container.appendChild(episodeNumber);
     }
     /////create episode image/////////
     let episodeImage = document.createElement("img");
-    if (allShows[i].image !== null) {
-      episodeImage.src = `${allShows[i].image.medium}`;
+    if (shows[i].image !== null) {
+      episodeImage.src = `${shows[i].image.medium}`;
     }
     episodeImage.className = "image";
     container.appendChild(episodeImage);
     /////create summary///////
-    let Showsummary = document.createElement("p");
-    Showsummary.innerHTML = `${allShows[i].summary}`;
-    container.appendChild(Showsummary);
+    let showSummary = document.createElement("p");
+    showSummary.innerHTML = `${shows[i].summary}`;
+    container.appendChild(showSummary);
   }
 }
 
-displayEpisodes(allShows);
+displayEpisodes(allEpisodes);
 
+////search bar event/////
+
+function filterSearch(event) {
+  let searchWord = event.target.value.toUpperCase();
+  let filteredShows = allEpisodes.filter((item) => {
+    return (
+      item.name.toUpperCase().includes(searchWord) ||
+      item.summary.toUpperCase().includes(searchWord)
+    );
+  });
+  displayEpisodes(filteredShows);
+}
+searchBar.addEventListener("input", filterSearch);
+
+/////link to TvMaze//////
 let linkToTv = document.createElement("a");
 linkToTv.href = "https://www.tvmaze.com/";
 let textToTv = document.createTextNode("Source");
